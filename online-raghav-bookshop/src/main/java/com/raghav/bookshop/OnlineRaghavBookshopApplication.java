970e25a02aff8b76495037e5f46ea675d1a9eca8
raghavendra <email8raghav@gmail.com>
2020-03-30 12:34:58 +0530

    online raghav bookshop

---
+++ online-raghav-bookshop/src/main/java/com/raghav/bookshop/OnlineRaghavBookshopApplication.java
@@ -0,0 +1,13 @@
+package com.raghav.bookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRaghavBookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineRaghavBookshopApplication.class, args);
+	}
+
+}
---
+++ online-raghav-bookshop/src/test/java/com/raghav/bookshop/OnlineRaghavBookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghav.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRaghavBookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
